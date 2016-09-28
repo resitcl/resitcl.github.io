f035a8b8bee8be9a58a44964a6251dec4cd4be93
samarti <samarti@uc.cl>
2016-09-28 17:10:49 -0300

    Added used technologies

--- js/script.js
+++ js/script.js
@@ -16,6 +16,14 @@
 			centerMode: true,
 			variableWidth: true
 		});
+
+		$('.grid').masonry({
+		  // set itemSelector so .grid-sizer is not used in layout
+		  itemSelector: '.grid-item',
+		  // use element for option
+		  columnWidth: '.grid-sizer',
+		  percentPosition: true
+		})
 	})
 
 	var $window = $(window),
