a7e7fb4a4055bb1d3768bc23ab4d405bc3e0afe7
samarti <samarti@uc.cl>
2017-01-27 17:49:27 -0300

    Updated position

--- js/script.js
+++ js/script.js
@@ -392,7 +392,7 @@
 	function initialize(){
 
 		var map_canvas = document.getElementById('map');
-		var myLatlng = new google.maps.LatLng(-33.417067, -70.592182); // set your location here
+		var myLatlng = new google.maps.LatLng(-33.413720, -70.573649); // set your location here
 		var map_options = {
 			center: myLatlng,
 			zoom: 16,
